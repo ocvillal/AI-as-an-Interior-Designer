aaaac3522f446cfe175540f24a6ceb9a307f5c2b
dinoplane <isacsac2017@gmail.com>
2023-08-16 21:25:48 -0700

    fixed some bugs in the starter

--- Assets/Scripts/Level/LevelGene.cs
+++ Assets/Scripts/Level/LevelGene.cs
@@ -4,48 +4,50 @@ using UnityEngine;
 
 // Finish by Thursday
 
-using Feature = (string, float, float); // name of the furniture, x, y
+
+using Feature = System.Tuple<string, float, float>; // name of the furniture, x, y
 
 // Represents an individual inside the population
-class LevelGene {
+public class LevelGene {
 
     //static // What would the json load in?
 
-    Array<Feature> features;
+    List<Feature> features;
 
     bool isValid;
 
-    void LevelGene(){
-        features = new Array<Feature>();
+    LevelGene(){
+        features = new List<Feature>();
     }
 
     static void LoadAllFurniture(){ // Arrian
 
     }
 
-    static LevelData GenerateRandomLevelData(){ // Arrian
-
+    static LevelGene GenerateRandomLevelGene(){ // Arrian
+        return new LevelGene();
     }
 
-    static LevelData GenerateEmptyLevelData(){ // Angela
-
+    static LevelGene GenerateEmptyLevelGene(){ // Angela
+        return new LevelGene();
     }
 
 
     float Fitness(){ // THE MEAT (HOw would we define this???? Someone research) Angela
-
+        return 0.0f;
     }
 
     bool ValidateSelf(){ // n^2 complexity
-
+        return true;
     }
 
     bool ValidateFeatureAddition(Feature feat, bool allowDuplicates){ // Can I add this item into myself? O(n) complexity // THE MEAT // Arrian
         // go through every piece of furniture in the features
         // validate with the rest of the
+        return false;
     }
 
-    LevelData Mutate(){ // A mutate function // Angela
+    LevelGene Mutate(){ // A mutate function // Angela
         // Either
 
         // Modifies the placement of an item
@@ -53,22 +55,25 @@ class LevelGene {
         // Adds a new item
 
         // Removes an item
+        return new LevelGene();
     }
 
-    LevelData PlaceObject(){ // Alan
-
+    LevelGene PlaceObject(){ // Alan
+        return new LevelGene();
     }
 
-    LevelData RemoveObject(){ // Octavio
-
+    LevelGene RemoveObject(){ // Octavio
+        return new LevelGene();
     }
 
-    Array<LevelData> GenerateChildren(LevelData other) { // Alan
+    List<LevelGene> GenerateChildren(LevelGene other) { // Alan
         // crossover
 
 
         // validation is needed
         // Consider feasible and infeasible population?
+        return new List<LevelGene>();
+
     }
 
 }
